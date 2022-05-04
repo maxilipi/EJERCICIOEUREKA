b956433a3983d9fa80cb0f4683f1c42ed78b6019
maxilipi <104212047+maxilipi@users.noreply.github.com>
2022-05-04 13:09:38 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+let eureka : string  = "eureka"; 
+let claveingresada: string = prompt ("clave ingresada"));
+
+if (eureka === claveingresada) { 
+ console.log("Correcto"); 
+} else { 
+ console.log("Incorrecto"); 
+} 
+
